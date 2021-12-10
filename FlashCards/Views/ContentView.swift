01e1cea01cd85962711f2b22659b428be6873602
winstonwang <wwang@lcs.on.ca>
2021-12-10 11:41:15 -0500

    Got a static interface start showing then questions and a button to check the answer.

--- FlashCards/Views/ContentView.swift
+++ FlashCards/Views/ContentView.swift
@@ -9,13 +9,33 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ScrollView{
+            // Show questions
+            Text("What is the \"prowerhouse\" of a cell?")
+                .font(.largeTitle)
+                .multilineTextAlignment(.center)
+            
+            //Check answer
+            Button(action: {
+                // NOTE: Output will not be shown unless this app is run in the "full" simulator
+                print("Button was pressed")
+            }, label: {
+                Text("Check")
+            })
+                .buttonStyle(.plain)
+            
+        }
+        .padding()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        if #available(iOS 15.0, *) {
+            ContentView()
+                .previewInterfaceOrientation(.landscapeLeft)
+        } else {
+            // Fallback on earlier versions
+        }
     }
 }
