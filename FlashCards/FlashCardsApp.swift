612288e6f3873e2ce6b0fd352f74836a3eac495f
winstonwang <wwang@lcs.on.ca>
2021-12-10 11:11:54 -0500

    Initial Commit

---
+++ FlashCards/FlashCardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashCardsApp.swift
+//  FlashCards
+//
+//  Created by Winston Wang on 2021-12-10.
+//
+
+import SwiftUI
+
+@main
+struct FlashCardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
